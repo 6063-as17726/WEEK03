c716a5b06451ef42538a94bfa2db89a8d4eb1e3a
Ayushi Shah <as17726@nyu.edu>
2023-09-20 16:50:19 -0400

    if statements

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # p5.js Template
 
+Image on repo:
+
 This is a README file that can be used to describe and document your assignment.
 
 Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
--- sketch.js
+++ sketch.js
@@ -1,5 +1,30 @@
+let xpos = 100; 
+
 function setup() {
+    createCanvas(600,600); 
 }
 
 function draw() {
+    background(200,20,20); 
+    //rect(xpos, 10,30,30); 
+    //rect(xpos, 50,30,30); 
+    //rect(xpos, 90,30,30); 
+    
+    if (mouseX> width/2)
+    {
+    if(mouseY>height/2)
+    {
+    fill(20,10,20);
+    } 
+    else
+    {fill(0,255,0);}
+}
+if(mouseX<width/2)
+{
+    if(mouseY>height/2)
+    {fill(0,50,0);}
+    else{fill(0,0,255);}
+}
+
+rect(mouseX,mouseY,30,30);
 }
